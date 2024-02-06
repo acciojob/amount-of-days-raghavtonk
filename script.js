0838a268cef65a8231f5eb996bbde222f9b3a4c6
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-06 13:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year % 400 == 0 || year % 100 !== 0 && year % 4 == 0){
+		return 366;
+	}
+	else
+		return 365;
+}
+let year = prompt("enter the year");
+alert(daysOfAYear(year));     
